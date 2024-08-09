288795280ae9593f47835e27128bb44f73e1c029
Jal7823 <jal7823@gmail.com>
2024-08-09 16:53:42 +0200

    feat: :zap: Start projects
    check TODO list

--- apps/ingredients/models.py
+++ apps/ingredients/models.py
@@ -3,3 +3,14 @@ from django.db import models
 class Ingredients(models.Model):
     name = models.CharField('Nombre', max_length=50)
     price = models.FloatField('Precio',default=0.0,null=True,blank=True)
+
+
+#TODO YOU NEED FINISH THE MODEL
+    def __str__(self):
+        return {self.name}
+
+    class Meta:
+        """Meta definition for Products."""
+
+        verbose_name = 'Drinks'
+        verbose_name_plural = 'Drinks'
