3d9110e5a842bcc92345dc737099b0a0ece04347
Jal7823 <jal7823@gmail.com>
2024-08-10 03:03:04 +0200

    feat: :heavy_plus_sign: Add CORS
    Add CORS only for localhost

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.drinks',
     'apps.ingredients',
     'apps.plates',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,6 +131,12 @@ STATIC_URL = 'static/'
 
 
 AUTH_USER_MODEL = "users.Users"
+
+#CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
